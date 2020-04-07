1d4762e48f50c683ac7db15e535525ec4f0f7ec6
Andy <andypicke@gmail.com>
2020-04-07 16:33:35 -0600

    Start script for cpj deaths analysis

---
+++ src/cpj_deaths_script.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# make plots look nice
+plt.rcParams['font.size'] = 14
+plt.rcParams['axes.labelsize'] = 'large'
+plt.rcParams['xtick.labelsize'] = 'large'
+plt.rcParams['ytick.labelsize'] = 'large'
+plt.rcParams['lines.linewidth'] = 3
+plt.style.use('ggplot')
+
+def get_unique_category_vals(df, col_name, dropna=False):
+    '''
+    Find unique category values for specified column in a dataframe, where some rows 
+    contain multiple categories separated by commas.
+    '''
+    unique_entries = list(cpj[col_name].unique())
+    unique_entries = list(map(str,unique_entries))
+    unique_entries_split = list(map(lambda x: x.split(','),unique_entries))
+    unique_vals = set([val for sublist in unique_entries_split for val in sublist])
+    if dropna:
+        unique_vals.remove('nan')
+    return(unique_vals)
+
+
+def add_category_counts_to_df(df, col_name, dropna=False):
+    '''
+    Add boolean columns to dataframe for each unique category in col_name indicating if that category 
+    was present in col_name. Useful when some rows of col_name contain multiple categories separated by commas.
+    Adding these columns allows us to sum/count each individual category separately.
+    '''
+    df_new = df.copy()
+    unique_vals = get_unique_category_vals(df_new, col_name, dropna)
+    
+    for val in unique_vals:
+        df_new[col_name + '_' + val] = df_new[col_name].apply(str).apply(lambda x: x.split(',')).apply(lambda x: val in x)
+    return df_new
+
+
+def count_categories(df, col_name, dropna=False):
+    '''
+    '''
+    df_count=add_category_counts_to_df(df, col_name, dropna)
+    df_count=df_count.filter(regex='^' + col_name, axis=1).sum().to_frame(name='Count').reset_index().rename(columns={'index':col_name}).sort_values('Count',ascending=False)
+    df_count[col_name]=df_count[col_name].apply(lambda x: x.replace(col_name +'_',''))
+    return df_count
+
+
+def plot_hbar_category_count_multiplecats(df, col_name, topN=None, dropna=False):
+    '''
+    Make a horizontal bar chart of category counts for a specified column in dataframe
+    '''
+    df_count = count_categories(df, col_name, dropna)
+    _, ax = plt.subplots(1,figsize=(12,8))
+    ax.barh(df_count[col_name], df_count['Count'])
+    ax.set_xlabel('# Journalists Killed')
+    if topN:
+        ax.set_title('# Journalists Killed By ' + col_name + ' (Top ' + str(topN) +')')
+    else:
+        ax.set_title('# Journalists Killed By ' + col_name)
+    ax.invert_yaxis()
+    return 
+
+
+def category_count_df_one_column(df, col_name, topN=None ,dropna=False):
+    '''
+    Make dataframe with category counts for a specified column
+    
+    INPUT
+    df : Pandas Dataframe
+    col_name : (str) Name of column to count
+    drop_na : (bool) (optional) Whether or not to include NaNs in counts
+    
+    OUTPUT
+    df_count : Pandas dataframe with 2 columns: col_name and Count
+    '''
+    df_count = cpj[col_name].value_counts(dropna).to_frame().reset_index().rename(columns={col_name:'Count','index':col_name})
+    if topN:
+        df_count = df_count.head(topN)
+    return df_count
+
+
+def plot_hbar_category_count(df, col_name, topN=None, dropna=False):
+    '''
+    Make a horizontal bar chart of category counts for a specified column in dataframe
+    '''
+    df_count = category_count_df_one_column(df, col_name, topN, dropna=False)
+    _, ax = plt.subplots(1,figsize=(12,8))
+    ax.barh(df_count[col_name], df_count['Count'])
+    ax.set_xlabel('# Journalists Killed')
+    if topN:
+        ax.set_title('# Journalists Killed By ' + col_name + ' (Top ' + str(topN) +')')
+    else:
+        ax.set_title('# Journalists Killed By ' + col_name)
+    ax.invert_yaxis()
+    return 
+
+
+if __name__=='__main__':
+
+    cpj = pd.read_csv('./data/Journalists Killed between 1992 and 2020.csv')
+    cpj.dropna(axis=1, how='all', inplace=True)
+    cpj.dropna(axis=1, thresh=1000, inplace=True)
+    cpj.drop(['status','type','employedAs','location','locality'], axis=1, inplace=True)
+
+
+    # Plot Number of Journalists killed per year
+    cpj_GB_year_count = category_count_df_one_column(cpj,'year')
+
+    _, ax = plt.subplots(1,figsize=(12,4))
+    ax.bar(cpj_GB_year_count['year'],cpj_GB_year_count['Count'])
+    ax.set_xlabel('Year')
+    ax.set_xlabel('# Journalists Killed')
+    ax.set_title('# Journalists Killed Per Year')
+
+    plt.savefig('./images/TotalDeathsVsYear.png', bbox_inches='tight')
